4bfcb4a39c0166db2054aee9c4034546a211c6c8
Evandro Farias <evandrofariasjr@gmail.com>
2022-08-09 17:40:53 -0300

    Transaction accept method added

--- src/main/java/com/app/backend/controllers/TransactionController.java
+++ src/main/java/com/app/backend/controllers/TransactionController.java
@@ -2,6 +2,7 @@ package com.app.backend.controllers;
 
 import com.app.backend.customException.PendingTransactionException;
 import com.app.backend.customException.RedundancyExcpetion;
+import com.app.backend.customException.UnreachableTransactionException;
 import com.app.backend.dtos.transaction.TransactionCreationDTO;
 import com.app.backend.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import java.util.UUID;
 @RequestMapping(value = "/transaction")
 public class TransactionController {
 
+    private final Boolean bulean = true;
     @Autowired
     private TransactionService transactionService;
 
@@ -39,4 +41,24 @@ public class TransactionController {
                     .body(e.getMessage());
         }
     }
+
+    @PatchMapping("/accept/{seller}")
+    public ResponseEntity<Object> acceptTransaction(@RequestParam("transaction") UUID transactionId,
+                                                    @PathVariable("seller") UUID sellerId) {
+        try {
+            return ResponseEntity.ok()
+                    .body(transactionService.acceptTransaction(sellerId, transactionId));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(404)
+                    .body(e.getMessage());
+        } catch (IllegalArgumentException | UnreachableTransactionException e) {
+            return ResponseEntity.status(400)
+                    .body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500)
+                    .body(e.getMessage());
+        }
+    }
+
+
 }
--- src/main/java/com/app/backend/services/TransactionService.java
+++ src/main/java/com/app/backend/services/TransactionService.java
@@ -3,6 +3,7 @@ package com.app.backend.services;
 import com.app.backend.customException.NotActiveException;
 import com.app.backend.customException.PendingTransactionException;
 import com.app.backend.customException.RedundancyExcpetion;
+import com.app.backend.customException.UnreachableTransactionException;
 import com.app.backend.dtos.transaction.TransactionCreationDTO;
 import com.app.backend.dtos.transaction.TransactionViewDTO;
 import com.app.backend.enums.TransactionStatus;
@@ -19,6 +20,7 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -39,7 +41,7 @@ public class TransactionService {
             User seller = userRepository.findById(transactionCreationDTO.getSellerId()).orElse(null);
             Product product = productRepository.findById(transactionCreationDTO.getProductId()).orElse(null);
 
-            this.isValidTransaction(buyer,seller,product);
+            this.isValidTransactionCreation(buyer, seller, product);
 
             Transaction transaction = Transaction.builder()
                     .buyer(buyer)
@@ -49,14 +51,14 @@ public class TransactionService {
                     .status(TransactionStatus.PENDING)
                     .build();
 
-            if(buyer.getBalance() < product.getPrice()){
+            if (buyer.getBalance() < product.getPrice()) {
                 transaction.setStatus(TransactionStatus.REJECTED);
                 transaction.setLastUpdateDate(new Date(System.currentTimeMillis()));
             }
 
             transactionRepository.save(transaction);
             return TransactionViewDTO.modelToDto(transaction);
-        } catch (Exception e){
+        } catch (Exception e) {
             log.error("Error creating transaction at Transaction Service");
             e.printStackTrace();
             throw e;
@@ -64,28 +66,79 @@ public class TransactionService {
 
     }
 
-    private void isValidTransaction(User buyer, User seller, Product product) throws Exception{
+    public TransactionViewDTO acceptTransaction(UUID sellerId, UUID transactionId) throws Exception {
+        try {
+            User seller = userRepository.findById(sellerId).orElse(null);
+            Transaction transaction = transactionRepository.findById(transactionId).orElse(null);
+            User buyer = transaction.getBuyer();
+
+            this.isValidTransactionAcceptance(transaction, seller);
+
+            if (buyer.getBalance() < transaction.getPrice()) {
+                transaction.setStatus(TransactionStatus.REJECTED);
+            } else {
+                Double price = transaction.getPrice() - (transaction.getPrice() * transaction.getProduct().getDiscount());
+                buyer.setBalance(buyer.getBalance() - price);
+                seller.setBalance(seller.getBalance() + price);
+                transaction.setStatus(TransactionStatus.APPROVED);
+
+                userRepository.save(buyer);
+                userRepository.save(seller);
+            }
+
+            transaction.setLastUpdateDate(new Date(System.currentTimeMillis()));
+
+            return TransactionViewDTO.modelToDto(transactionRepository.save(transaction));
+
+        } catch (Exception e) {
+            log.error("Error accepting transaction at Transaction Service");
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    private void isValidTransactionCreation(User buyer, User seller, Product product) throws Exception {
         if (buyer == null
                 || seller == null
-                || product == null){
+                || product == null) {
             String message = "Invalid ";
             message += buyer == null ? "buyer " : "";
             message += seller == null ? "seller " : "";
             message += product == null ? "product" : "";
             throw new NoSuchElementException("Operation invalid. " + message);
         }
-        if(!buyer.getIsActive() || !seller.getIsActive()){
+        if (!buyer.getIsActive() || !seller.getIsActive()) {
             throw new NotActiveException("Buyer or Seller is not active");
         }
         Optional<Transaction> toValidate = transactionRepository.findByProductId(product.getId());
-        if(toValidate.isPresent() && toValidate.get().getStatus() == TransactionStatus.PENDING){
+        if (toValidate.isPresent() && toValidate.get().getStatus() == TransactionStatus.PENDING) {
             throw new PendingTransactionException("There is a pending transaction for this product already");
         }
-        if(product.getUser() != seller){
+        if (product.getUser() != seller) {
             throw new Exception("Product seller is incorrect");
         }
-        if(buyer == seller){
+        if (buyer == seller) {
             throw new RedundancyExcpetion("Buyer and seller are the same user");
         }
     }
+
+    public void isValidTransactionAcceptance(Transaction transaction, User seller) throws Exception {
+
+        if (transaction == null) {
+            throw new NoSuchElementException("Transaction not found");
+        }
+        if (seller == null) {
+            throw new NoSuchElementException("User not found");
+        }
+        if (transaction.getSeller() != seller) {
+            throw new IllegalArgumentException("Incorrect seller for the transaction");
+        }
+        if (transaction.getStatus() == TransactionStatus.APPROVED) {
+            throw new UnreachableTransactionException("Transaction have already been approved");
+        }
+        if (transaction.getStatus() != TransactionStatus.PENDING) {
+            throw new UnreachableTransactionException("Transaction status doesn't qualify for approval");
+        }
+    }
+
 }
