6bda377b663f04f51db1a587df50c4141cb82e3c
Evandro Farias <evandrofariasjr@gmail.com>
2022-08-10 16:39:07 -0300

    push test

--- src/main/java/com/app/backend/controllers/TransactionController.java
+++ src/main/java/com/app/backend/controllers/TransactionController.java
@@ -42,7 +42,7 @@ public class TransactionController {
         }
     }
 
-    @PatchMapping("/proccess/{seller}")
+    @PatchMapping("/proccess/{sellerr}")
     public ResponseEntity<Object> proccessTransaction(@RequestParam("transaction") UUID transactionId,
                                                       @RequestParam("status") String action,
                                                       @PathVariable("seller") UUID sellerId) {
