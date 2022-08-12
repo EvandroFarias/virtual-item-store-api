52a78d24fb251cbe336328478ee17494dcccec65
Evandro Farias <evandrofariasjr@gmail.com>
2022-08-11 23:45:19 -0300

    removed test remains

--- src/main/java/com/app/backend/BackendApplication.java
+++ src/main/java/com/app/backend/BackendApplication.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication()
-@Api(value = "Testando")
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
